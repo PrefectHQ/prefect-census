1cbac84968c7cff511e3b4d00430cee38de8446d
Serina Grill <serinagrill@gmail.com>
2022-10-21 17:04:06 -0700

    Update syncs, add runs

--- prefect_census/__init__.py
+++ prefect_census/__init__.py
@@ -1,6 +1,7 @@
 from . import _version
 from .credentials import CensusCredentials
 from .client import CensusClient
-from .syncs import get_census_sync_run_info, trigger_census_sync
+from .syncs import trigger_census_sync
+from .runs import get_census_sync_run_info
 
 __version__ = _version.get_versions()["version"]
