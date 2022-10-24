c39f65bac97cdc46de910c92e0b7673c60ff3bf1
Serina Grill <serinagrill@gmail.com>
2022-10-24 10:22:13 -0700

    edited mock return

--- tests/test_runs.py
+++ tests/test_runs.py
@@ -1,3 +1,4 @@
+from prefect import flow
 import pytest
 from httpx import Response
 from prefect_census.runs import CensusGetSyncRunInfoFailed, get_census_sync_run_info
@@ -13,15 +14,14 @@ class TestGetCensusSyncRunInfo:
             credentials=census_credentials,
             run_id=42
         )
-        assert response == {"sync_run_id": 42}
 
     async def test_get_nonexistent_run(self, respx_mock, census_credentials):
         respx_mock.get(
                 "https://app.getcensus.com/api/v1/sync_runs/4",  # noqa
                 headers={"Authorization": f"Bearer my_api_key"},
-            ).mock(return_value=Response(404, json={"data": {"sync_run_id": 4}})
+            ).mock(return_value=Response(404, json={"status": {"user_message": "Not found!"}})
         )
-        with pytest.raises(CensusGetSyncRunInfoFailed):
+        with pytest.raises(CensusGetSyncRunInfoFailed, match="Not found!"):
             await get_census_sync_run_info.fn(
                 credentials=census_credentials,
                 run_id=4
