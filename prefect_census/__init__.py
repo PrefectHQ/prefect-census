3b864724c1080f82f664c52883bb1f3becaffa99
Serina Grill <serinagrill@gmail.com>
2022-10-24 10:47:15 -0700

    add noqa

--- prefect_census/__init__.py
+++ prefect_census/__init__.py
@@ -1,7 +1,7 @@
 from . import _version
-from .client import CensusClient
-from .credentials import CensusCredentials
-from .runs import get_census_sync_run_info
-from .syncs import trigger_census_sync
+from .client import CensusClient  # noqa
+from .credentials import CensusCredentials  # noqa
+from .runs import get_census_sync_run_info  # noqa
+from .syncs import trigger_census_sync  # noqa
 
 __version__ = _version.get_versions()["version"]
