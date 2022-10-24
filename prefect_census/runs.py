f66b1f4f85a4ffa92adbaeebcfe637ca151a8825
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2022-10-24 14:24:52 -0700

    Update prefect_census/runs.py
    Co-authored-by: Andrew <15331990+ahuang11@users.noreply.github.com>

--- prefect_census/runs.py
+++ prefect_census/runs.py
@@ -35,7 +35,6 @@ class CensusGetSyncRunInfoFailed(RuntimeError):
 class CensusSyncRunCancelled(Exception):
     """Raised when a triggered sync run is cancelled"""
 
-    pass
 
 
 class CensusSyncRunStatus(Enum):
